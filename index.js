bc9d5daeb8e3757f0b36af12113ff9c6569510f4
Johi <155817168+fohij@users.noreply.github.com>
2024-02-06 14:30:47 +0100

    first
    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let python = document.getElementById("python");
+let heading = document.getElementById("am-heading");
+
+heading.onmouseover = function(){
+    heading.style.backgroundColor="blue";
+}
+
+// let menuitem = 
+python.onmouseover = function (){
+    menuitem.style.transform= "width: 150px";
+}
