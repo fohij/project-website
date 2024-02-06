bb9c7432fe5cb27f07dcb190bc2cd277237820f7
Johi <155817168+fohij@users.noreply.github.com>
2024-02-06 15:11:12 +0100

    Erste änderung im Javas

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const divs = document.querySelectorAll('.projects div');
 
+divs.forEach(div => {
+    div.addEventListener('mouseover', () => {
+        div.style.transform = 'scale(1.2)';
+    });
+
+    div.addEventListener('mouseout', () => {
+        div.style.transform = 'scale(1)';
+    });
+});
